25e463aa05c4ffb9d85627a2ff70ebf4dd96216e
bakuzan <imannoyed4days@hotmail.co.uk>
2018-06-07 14:16:19 +0100

    classes!

---
+++ classes.py
@@ -0,0 +1,19 @@
+
+class Fish:
+
+    def __init__(self, type):
+        self.type = type
+        self.moving = False
+
+    def swim(self):
+        self.moving = True
+        print("{} is swimming".format(self.type))
+
+f = Fish('goldfish')
+g = Fish('great white')
+
+f.swim()
+print(f.__class__)
+print(f.moving)
+
+print(g.moving)
